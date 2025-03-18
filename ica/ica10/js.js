ec0ca3b0aba228779b67f78c37d4f5444cc6baea
Anika Seter <anse5475@colorado.edu>
2025-03-18 12:53:44 -0600

    finished ica10

--- ica/ica10/js.js
+++ ica/ica10/js.js
@@ -3,7 +3,9 @@ let stopText = document.getElementById('stopText');
 let spinningPhoto = document.getElementById('spinningPhoto');
 let isButtonSpinning = true;
 let sleepButton = document.getElementById('sleep');
+let sleepText = document.getElementById('sleepText');
 let darkness = document.getElementById('darkness');
+let isDark = false;
 
 stopButton.addEventListener('click', pausePhoto);
 sleepButton.addEventListener('click', sleep);
@@ -11,16 +13,29 @@ sleepButton.addEventListener('click', sleep);
 function pausePhoto() {
     if(isButtonSpinning) {
         spinningPhoto.style.animationPlayState = 'paused';
+        spinningPhoto.querySelector('img').style.animationPlayState = 'paused';
         stopText.textContent = 'Start the Photo';
         isButtonSpinning = false;
     }
     else if(!isButtonSpinning) {
         spinningPhoto.style.animationPlayState = 'running';
+        spinningPhoto.querySelector('img').style.animationPlayState = 'running';
         stopText.textContent = 'Stop the Photo';
         isButtonSpinning = true;
     }
 }
 
 function sleep() {
-    darkness.style.display = "contents";
+    if(!isDark) {
+        darkness.style.display = 'unset';
+        sleepText.textContent = 'Wake Up';
+        sleepText.style.backgroundColor = 'rgb(5, 20, 31)';
+        isDark = true;
+    }
+    else if(isDark) {
+        darkness.style.display = 'none';
+        sleepText.textContent = 'Go to Sleep';
+        sleepText.style.backgroundColor = 'rgb(33, 97, 140)';
+        isDark = false;
+    }
 }
